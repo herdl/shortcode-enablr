3603bf4242172dca9454e42a502897524e8989cd
B3none <ablackham2000@gmail.com>
2019-12-19 14:57:55 +0000

    version bump

--- shortcode-enablr.php
+++ shortcode-enablr.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/herdl/shortcode-enablr
  * Description: Shortcode enabler.
  * Author: Herdl
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author URI: https://herdl.com
  */
 
